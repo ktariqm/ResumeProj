88e2db63ca31443f55c2d893847fd7867f2b8e6e
Khurram Tariq <khurramtm@gmail.com>
2017-04-14 01:38:21 -0700

    Updated Port

--- resout.py
+++ resout.py
@@ -15,5 +15,7 @@ def usern(candidate):
         return(json.dumps(resume_data, indent=4))
 
 if __name__ == "__main__":
-    app.run()
+#    app.run()
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
 
